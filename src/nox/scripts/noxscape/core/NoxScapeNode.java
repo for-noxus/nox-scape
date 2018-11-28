8c168355980113afec856716238e22b6d4afab85
Nox <nox.scripts@gmail.com>
2018-11-28 00:43:35 -0500

    sllows nodes to have multiple childNodes, adds debug method to print node & all children nodes

--- src/nox/scripts/noxscape/core/NoxScapeNode.java
+++ src/nox/scripts/noxscape/core/NoxScapeNode.java
@@ -2,20 +2,33 @@ package nox.scripts.noxscape.core;
 
 import nox.api.graphscript.Node;
 
+import java.util.List;
+import java.util.Objects;
+
 public abstract class NoxScapeNode<k extends Tracker> extends Node<NoxScapeNode> {
 
     protected final ScriptContext ctx;
 
     private k tracker;
 
-    public NoxScapeNode(ScriptContext ctx, k tracker) {
-        this(null, ctx, null, tracker);
-    }
-
-    public NoxScapeNode(NoxScapeNode next, ScriptContext ctx, String message, k tracker) {
-        super(next, message);
+    public NoxScapeNode(List<NoxScapeNode> children, ScriptContext ctx, String message, k tracker) {
+        super(children, message);
         this.ctx = ctx;
         this.tracker = tracker;
+    }
+
+    public NoxScapeNode(NoxScapeNode child, ScriptContext ctx, String message, k tracker) {
+        super(child, message);
+        this.ctx = ctx;
+        this.tracker = tracker;
+    }
+
+    public String toDebugString() {
+        String ret = "Message: " + this.getMessage();
+        if (this.getChildNodes() != null && this.getChildNodes().size() > 0) {
+            ret += "\nChildren: " +  this.getChildNodes().stream().filter(Objects::nonNull).map(m -> m.getClass().getSimpleName()).reduce("", (a, b) -> a + b + ", ");
+        }
+        return ret;
     }
 
     @Override
