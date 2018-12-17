5d0914727b5745d0d40ef952590e8ec2df99e493
Nox <nox.scripts@gmail.com>
2018-12-17 02:16:45 -0500

    finishes entities; adds producing item as well

--- src/nox/scripts/noxscape/tasks/mining/MiningEntity.java
+++ src/nox/scripts/noxscape/tasks/mining/MiningEntity.java
@@ -1,29 +1,36 @@
 package nox.scripts.noxscape.tasks.mining;
 
+import nox.scripts.noxscape.core.interfaces.IInteractable;
 import nox.scripts.noxscape.core.interfaces.ISkillable;
+import org.osbot.rs07.api.map.Position;
 import org.osbot.rs07.api.model.Entity;
 import org.osbot.rs07.api.ui.Skill;
+import org.osbot.rs07.script.MethodProvider;
 
-public enum MiningEntities implements ISkillable {
-    CLAY("Clay", new short[]{6705}, 1),
-    COPPER("Copper", new short[]{4645, 4510}, 1),
-    TIN("Tin", new short[]{53}, 1),
-    IRON("Iron", new short[]{2576}, 15),
-    SILVER("Silver", new short[]{74}, 20),
-    COAL("Coal", new short[]{10508}, 30),
-    GOLD("Gold", new short[]{8885}, 40),
-    MITHRIL("Mithril", new short[]{-22239}, 55),
-    ADAMANTITE("Adamantite", new short[]{21662}, 70),
-    RUNITE("Runite", new short[]{-31437}, 85);
+import java.util.Arrays;
+
+public enum MiningEntity implements ISkillable, IInteractable {
+    CLAY("Clay", new short[]{6705}, 1, "Clay"),
+    COPPER("Copper", new short[]{4645, 4510}, 1, "Copper ore"),
+    TIN("Tin", new short[]{53}, 1, "Tin ore"),
+    IRON("Iron", new short[]{2576}, 15, "Iron ore"),
+    SILVER("Silver", new short[]{74}, 20, "Silver ore"),
+    COAL("Coal", new short[]{10508}, 30, "Coal"),
+    GOLD("Gold", new short[]{8885}, 40, "Gold ore"),
+    MITHRIL("Mithril", new short[]{-22239}, 55, "Mithril ore"),
+    ADAMANTITE("Adamantite", new short[]{21662}, 70, "Adamantite ore"),
+    RUNITE("Runite", new short[]{-31437}, 85, "Runite ore");
 
     private String name;
     private short[] modifiedColors;
     private int requiredLevel;
+    private String producesItem;
 
-    MiningEntities(String name, short[] modifiedColors, int requiredLevel) {
+    MiningEntity(String name, short[] modifiedColors, int requiredLevel, String producesItem) {
         this.name = name;
         this.modifiedColors = modifiedColors;
         this.requiredLevel = requiredLevel;
+        this.producesItem = producesItem;
     }
 
     /*
@@ -72,7 +79,21 @@ public enum MiningEntities implements ISkillable {
 
     @Override
     public String producesItemName() {
+        return producesItem;
+    }
+
+    @Override
+    public Position getPosition() {
         return null;
     }
 
+    @Override
+    public String toString() {
+        return "MiningEntity{" +
+                "name='" + name + '\'' +
+                ", modifiedColors=" + Arrays.toString(modifiedColors) +
+                ", requiredLevel=" + requiredLevel +
+                ", producesItem='" + producesItem + '\'' +
+                '}';
+    }
 }
