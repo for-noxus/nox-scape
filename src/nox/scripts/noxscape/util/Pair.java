f3aa0b8a489b55253777dc458d163ecbbf38a07a
Nox <nox.scripts@gmail.com>
2018-12-17 02:11:01 -0500

    adds public getters

--- src/nox/scripts/noxscape/util/Pair.java
+++ src/nox/scripts/noxscape/util/Pair.java
@@ -2,12 +2,14 @@ package nox.scripts.noxscape.util;
 
 public class Pair<k, v> {
 
-    public k a;
-    public v b;
+    public final k a;
+    public final v b;
 
     public Pair(k a, v b) {
         this.a = a;
         this.b = b;
     }
 
+    public k getA() { return a; }
+    public v getB() { return b; }
 }
