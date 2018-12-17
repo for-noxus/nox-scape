76f2b9b0e09c21f44f5c5d864f143332235fc4d8
Nox <nox.scripts@gmail.com>
2018-12-17 02:18:33 -0500

    fixes math error with levels

--- src/nox/scripts/noxscape/core/items/WoodcuttingItems.java
+++ src/nox/scripts/noxscape/core/items/WoodcuttingItems.java
@@ -10,7 +10,7 @@ public final class WoodcuttingItems {
         List<CachedItem> items = CachedItem.generateFromBaseMetals("axe", Skill.WOODCUTTING, 1, 1, 6, 11, 21, 31, 41, 61);
 
         for (CachedItem i: items) {
-            i.addLevelRequirement(Skill.ATTACK,  i.getLevelRequirement(Skill.WOODCUTTING) + 1);
+            i.addLevelRequirement(Skill.ATTACK,  i.getLevelRequirement(Skill.WOODCUTTING) - 1);
         }
 
         return items;
