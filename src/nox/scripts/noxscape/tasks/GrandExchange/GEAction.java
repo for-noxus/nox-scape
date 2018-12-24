d1e7fe6fa6bdca8cb2a6be92b20d212840868297
Nox <nox.scripts@gmail.com>
2018-12-24 15:54:10 -0500

    initial work on GE node

---
+++ src/nox/scripts/noxscape/tasks/GrandExchange/GEAction.java
@@ -0,0 +1,6 @@
+package nox.scripts.noxscape.tasks.GrandExchange;
+
+public enum GEAction {
+    BUY,
+    SELL
+}
---
+++ src/nox/scripts/noxscape/tasks/GrandExchange/GEItem.java
@@ -0,0 +1,7 @@
+package nox.scripts.noxscape.tasks.GrandExchange;
+
+public class GEItem {
+    private String name;
+    private GEAction action;
+    private int amount;
+}
