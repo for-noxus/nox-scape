61347fca71ea12f24dc69af3a5fde57632f3cfa3
Nox <Nox.scripts@gmail.com>
2018-12-26 14:14:47 -0500

    adds getters for all private fields

--- src/nox/scripts/noxscape/tasks/GrandExchange/GEItem.java
+++ src/nox/scripts/noxscape/tasks/GrandExchange/GEItem.java
@@ -4,4 +4,31 @@ public class GEItem {
     private String name;
     private GEAction action;
     private int amount;
+
+    public GEItem(String name, GEAction action, int amount) {
+        this.name = name;
+        this.action = action;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public GEAction getAction() {
+        return action;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "GEItem{" +
+                "name='" + name + '\'' +
+                ", action=" + action +
+                ", amount=" + amount +
+                '}';
+    }
 }
