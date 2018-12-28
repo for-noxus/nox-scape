59bfa530877edd050db9c468fc52d841ee648d31
Nox <nox.scripts@gmail.com>
2018-12-28 02:45:11 -0500

    adds INodeSupplier to convey that a class can supply MasterNodes

---
+++ src/nox/scripts/noxscape/core/interfaces/INodeSupplier.java
@@ -0,0 +1,7 @@
+package nox.scripts.noxscape.core.interfaces;
+
+import nox.scripts.noxscape.core.NoxScapeMasterNode;
+
+public interface INodeSupplier {
+     NoxScapeMasterNode getNextMasterNode();
+}
