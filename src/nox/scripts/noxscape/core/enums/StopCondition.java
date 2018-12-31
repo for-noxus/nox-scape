33a4f2b882cd8a50bebd062013fdcdd715603096
Nox <nox.scripts@gmail.com>
2018-12-31 03:39:48 -0500

    adds a default "Unset" condition to indicate no condition HAS been set

--- src/nox/scripts/noxscape/core/enums/StopCondition.java
+++ src/nox/scripts/noxscape/core/enums/StopCondition.java
@@ -3,6 +3,7 @@ package nox.scripts.noxscape.core.enums;
 import nox.scripts.noxscape.core.interfaces.INameable;
 
 public enum StopCondition implements INameable {
+    UNSET("Unset"),
     TIME_ELAPSED("Time Elapsed"),
     MONEY_MADE("Money Made"),
     LEVELS_GAINED("Levels Gained"),
