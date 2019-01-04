374bccf53887dc31429d552b51cbc8567cd13c53
Nox <nox.scripts@gmail.com>
2019-01-04 01:47:04 -0500

    brings woodcutting up-to-date with all the refactors brought from mining

--- src/nox/scripts/noxscape/tasks/woodcutting/WoodcuttingMasterNode.java
+++ src/nox/scripts/noxscape/tasks/woodcutting/WoodcuttingMasterNode.java
@@ -6,19 +6,21 @@ import nox.scripts.noxscape.core.enums.Frequency;
 import nox.scripts.noxscape.core.enums.MasterNodeType;
 import nox.scripts.noxscape.core.CachedItem;
 import nox.scripts.noxscape.core.enums.StopCondition;
+import nox.scripts.noxscape.core.interfaces.IActionListener;
 import nox.scripts.noxscape.tasks.base.BankingNode;
 import nox.scripts.noxscape.tasks.base.EntitySkillingNode;
 import nox.scripts.noxscape.tasks.base.WalkingNode;
 import nox.scripts.noxscape.tasks.base.banking.BankAction;
 import nox.scripts.noxscape.tasks.base.banking.BankItem;
+import nox.scripts.noxscape.tasks.base.banking.BankLocation;
 import org.osbot.rs07.api.map.Position;
+import org.osbot.rs07.api.model.Item;
 import org.osbot.rs07.api.ui.Message;
 import org.osbot.rs07.api.ui.Skill;
 import org.osbot.rs07.event.webwalk.PathPreferenceProfile;
+import org.osbot.rs07.listener.MessageListener;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class WoodcuttingMasterNode<k> extends NoxScapeMasterNode<WoodcuttingMasterNode.Configuration> {
@@ -36,12 +38,14 @@ public class WoodcuttingMasterNode<k> extends NoxScapeMasterNode<WoodcuttingMast
 
     @Override
     public boolean canExecute() {
-        return false;
+        return true;
     }
 
     @Override
     public void initializeNodes() {
-        BankItem[] axesToWithdraw = WoodcuttingItems.axes().stream().map(m -> new BankItem(m.getName(), BankAction.WITHDRAW, 1, "Woodcutting", m.requiredLevelSum(), true)).toArray(BankItem[]::new);
+
+        if (configuration == null)
+            configuration = new Configuration();
 
         // Get the highest level tree we can currently cut
         if (configuration.treeToChop == null)
@@ -55,12 +59,24 @@ public class WoodcuttingMasterNode<k> extends NoxScapeMasterNode<WoodcuttingMast
 
         // Get the closest WoodCutting location to ours
         final Position curPos = ctx.myPosition();
-        WoodcuttingLocation location = Arrays.stream(WoodcuttingLocation.values())
+        WoodcuttingLocation woodcuttingLocation = Arrays.stream(WoodcuttingLocation.values())
                 .filter(f -> f.containsTree(configuration.treeToChop))
                 .min(Comparator.comparingInt(a -> a.distanceToCenterPoint(curPos)))
                 .get();
 
+        BankItem[] axesToWithdraw = WoodcuttingItems.axes().stream()
+                .filter(f -> f.canUse(ctx))
+                .map(m -> {
+                    BankItem item = new BankItem(m.getName(), BankAction.WITHDRAW, 1, "Woodcutting", m.requiredLevelSum(), true);
+                    if (m.getLevelRequirement(Skill.WOODCUTTING) > 20)
+                        item.buyIfNecessary(1);
+                    return item;
+                })
+                .toArray(BankItem[]::new);
         BankItem logsToBank = new BankItem(configuration.treeToChop.producesItemName(), BankAction.DEPOSIT, 100);
+
+        List<BankItem> itemsToBank = new ArrayList<>(Arrays.asList(axesToWithdraw));
+        itemsToBank.add(logsToBank);
 
         PathPreferenceProfile ppp = new PathPreferenceProfile()
                 .checkBankForItems(true)
@@ -68,38 +84,39 @@ public class WoodcuttingMasterNode<k> extends NoxScapeMasterNode<WoodcuttingMast
                 .checkInventoryForItems(true)
                 .setAllowTeleports(true);
 
+        BankLocation preExecutionBankLocation = woodcuttingLocation.getBank().isDepositBox() ? BankLocation.closestTo(ctx, ctx.myPosition(), false) : woodcuttingLocation.getBank();
         NoxScapeNode preExecutionWalkNode = new WalkingNode(ctx)
                 .isWebWalk(true)
                 .setPathProfile(ppp)
-                .toArea(location.getBank().getBankArea())
-                .hasMessage(String.format("Walk to %s bank for the first time", location.getBank().getName()));
+                .toArea(preExecutionBankLocation.getBankArea())
+                .hasMessage(String.format("Walking to %s bank for the first time", preExecutionBankLocation.getName()));
 
         NoxScapeNode preExecutionBankNode = new BankingNode(ctx)
-                .bankingAt(location.getBank())
+                .bankingAt(preExecutionBankLocation)
                 .depositAllWornItems()
                 .depositAllBackpackItems()
                 .handlingItems(axesToWithdraw)
                 .hasMessage("Getting player ready to cut some trees");
 
         NoxScapeNode toTreeNode = new WalkingNode(ctx)
-                .toPosition(location.centerPoint())
+                .toPosition(woodcuttingLocation)
                 .isWebWalk(true)
                 .hasMessage("Walking to Trees (" + configuration.treeToChop.getName() + ")");
 
         NoxScapeNode toBankNode = new WalkingNode(ctx)
-                .toClosestBankFrom(location.getBank().getBankArea())
+                .toPosition(woodcuttingLocation.getBank())
                 .isWebWalk(true)
                 .hasMessage("Walking to Bank");
 
         NoxScapeNode bankNode = new BankingNode(ctx)
-                .bankingAt(location.getBank())
-                .handlingItems(logsToBank)
+                .bankingAt(woodcuttingLocation.getBank())
+                .handlingItems(itemsToBank)
                 .hasMessage("Banking " + configuration.treeToChop.producesItemName());
 
         NoxScapeNode interactNode = new EntitySkillingNode(ctx)
                 .interactWith(configuration.treeToChop)
-                .boundedBy(location.centerPoint(), 14)
-                .entityInvalidWhen(ent -> ctx.getObjects().closest(obj -> obj.getPosition().equals(ent.getPosition()) && obj.getName().equals("Tree stump")) != null, 5000, 1000)
+                .boundedBy(woodcuttingLocation.centerPoint(), 14)
+                .entityInvalidWhen(ent -> ent.getName().equals("Tree stump"), 30000, 1000)
                 .hasMessage("Chopping " + configuration.treeToChop.getName());
 
         toTreeNode.setChildNode(interactNode);
@@ -109,10 +126,11 @@ public class WoodcuttingMasterNode<k> extends NoxScapeMasterNode<WoodcuttingMast
         preExecutionBankNode.setChildNode(toTreeNode);
         preExecutionWalkNode.setChildNode(preExecutionBankNode);
 
-        setNodes(Arrays.asList(toTreeNode, interactNode, toBankNode, bankNode, preExecutionBankNode, preExecutionWalkNode));
+        setNodes(Arrays.asList(toTreeNode, interactNode, toBankNode, bankNode));
         setPreExecutionNode(preExecutionWalkNode);
         setReturnToBankNode(toBankNode);
 
+        ctx.getBot().addMessageListener(this);
         ctx.logClass(this, String.format("Initialized %d nodes.", getNodes().size()));
     }
 
@@ -130,7 +148,17 @@ public class WoodcuttingMasterNode<k> extends NoxScapeMasterNode<WoodcuttingMast
 
     @Override
     public void onMessage(Message message) throws InterruptedException {
-
+        if (message.getType() == Message.MessageType.GAME) {
+            if (message.getMessage().toLowerCase().contains("you get some")) {
+                Item item = ctx.getInventory().getItem(configuration.treeToChop.producesItemName());
+                if (item == null) {
+                    ctx.logClass(this, "Unable to log action for mining entity " + configuration.treeToChop);
+                }
+                else {
+                    ctx.getScriptProgress().onItemAcquired(item.getId(), 1);
+                }
+            }
+        }
     }
 
     public static class Configuration {
