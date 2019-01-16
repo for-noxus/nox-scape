d1eeafdb4c4c9a51a7fc1b1994561b91fb2a900b
Nox <nox.scripts@gmail.com>
2019-01-16 01:31:51 -0500

    removes lumbridge west

--- src/nox/scripts/noxscape/tasks/woodcutting/WoodcuttingLocation.java
+++ src/nox/scripts/noxscape/tasks/woodcutting/WoodcuttingLocation.java
@@ -17,7 +17,6 @@ public enum WoodcuttingLocation implements IBankable, INameable, ILocateable {
     FALADOR_N_OAKS("Oaks North of Falador", BankLocation.FALADOR_WEST, new Position(2951, 3403, 0), WoodcuttingEntity.OAK),
     PORT_SARIM_OAKS("Oaks North of Port Sarim", BankLocation.PORTSARIM, new Position(3039, 3264, 0), WoodcuttingEntity.OAK),
     DRAYNOR_OAKS("Oak trees directly east of Draynor Bank", BankLocation.DRAYNOR, new Position(3100, 3240, 0), WoodcuttingEntity.OAK),
-    LUMBRIDGE("Lumbridge, directly west of the castle.", BankLocation.LUMBRIDGE_UPPER, new Position(3190, 3220, 0), WoodcuttingEntity.TREE, WoodcuttingEntity.OAK),
     DRAYNOR_WILLOWS("Draynor willows by the river", BankLocation.DRAYNOR, new Position(3085, 3237, 0), WoodcuttingEntity.WILLOW),
     EDGEVILLE("Yews south of Edgeville bank", BankLocation.EDGEVILLE, new Position(3085, 3475, 0), WoodcuttingEntity.YEW),
     FALADOR_YEWS("Yews south of Falador", BankLocation.FALADOR_WEST, new Position[] { new Position(2998, 3313, 0), new Position(3018, 3316, 0), new Position(3041, 3320, 0) }, WoodcuttingEntity.YEW);
