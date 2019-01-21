666515582486f2bba89dcf95d886e16d353b7b24
Nox <nox.scripts@gmail.com>
2019-01-21 00:38:39 -0500

    filters out black pickaxe

--- src/nox/scripts/noxscape/tasks/mining/MiningItems.java
+++ src/nox/scripts/noxscape/tasks/mining/MiningItems.java
@@ -4,16 +4,17 @@ import nox.scripts.noxscape.core.CachedItem;
 import org.osbot.rs07.api.ui.Skill;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class MiningItems {
 
     public static List<CachedItem> pickaxes() {
-        List<CachedItem> items = CachedItem.generateFromBaseMetals("pickaxe", Skill.MINING, 1, 1, 6, 11, 21, 31, 41, 61);
+        List<CachedItem> items = CachedItem.generateFromBaseMetals("pickaxe", Skill.MINING, 1, 1, 6, -1, 21, 31, 41, 61);
 
         for (CachedItem i: items) {
             i.addLevelRequirement(Skill.ATTACK, i.getLevelRequirement(Skill.MINING) - 1);
         }
 
-        return items;
+        return items.stream().filter(f -> f.requiredLevelSum() > 0).collect(Collectors.toList());
     }
 }
