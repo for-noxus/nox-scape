c4fcef25d04dc9a719ae29fab1df3cd917c0d5ff
Nox <nox.scripts@gmail.com>
2019-01-24 01:16:10 -0500

    continues work on fishing

--- src/nox/scripts/noxscape/tasks/fishing/FishingEntity.java
+++ src/nox/scripts/noxscape/tasks/fishing/FishingEntity.java
@@ -1,4 +1,45 @@
 package nox.scripts.noxscape.tasks.fishing;
 
-public enum FishingEntity {
+import nox.scripts.noxscape.core.interfaces.INameable;
+
+public enum FishingEntity implements INameable {
+    NET("Netting", "Net", "Small fishing net"),
+    BAIT("Fishing", "Bait", "Fishing rod", "Fishing bait"),
+    FLY("Fly fishing", "Lure", "Fly fishing rod", "Feather"),
+    HARPOON("Harpooning", "Harpoon", "Harpoon"),
+    POT("Caging Lobs", "Cage", "Lobster pot");
+
+    private final String friendlyName;
+    private final String actionName;
+    private final String primaryItemName;
+
+    private final String secondaryItemName;
+
+    FishingEntity(String friendlyName, String actionName, String primaryItemName) {
+        this(friendlyName, actionName, primaryItemName, null);
+    }
+
+    FishingEntity(String friendlyName, String actionName, String primaryItemName, String secondaryItemName) {
+        this.friendlyName = friendlyName;
+        this.actionName = actionName;
+        this.primaryItemName = primaryItemName;
+        this.secondaryItemName = secondaryItemName;
+    }
+
+    @Override
+    public String getName() {
+        return friendlyName;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public String getPrimaryItemName() {
+        return primaryItemName;
+    }
+
+    public String getSecondaryItemName() {
+        return secondaryItemName;
+    }
 }
--- src/nox/scripts/noxscape/tasks/fishing/FishingItems.java
+++
@@ -1,4 +0,0 @@
-package nox.scripts.noxscape.tasks.fishing;
-
-public class FishingItems {
-}
